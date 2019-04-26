a7b4977c568a4d8cf2621dc8d6c498d17ac8dc5b
Roger Peixoto Duarte <duarterogerpeixoto@gmail.com>
2019-04-26 08:19:27 -0300

    Finished section 15

--- ReactCourse/burgerbuilder/src/containers/Checkout/ContactData/ContactData.js
+++ ReactCourse/burgerbuilder/src/containers/Checkout/ContactData/ContactData.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
 import Button from '../../../components/UI/Button/Button'
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import axios from '../../../axios-orders'
 import Input from '../../../components/UI/Input/Input'
+
 
 import classes from './ContactData.css'
 
@@ -106,7 +109,7 @@ class ContactData extends Component{
                formData[formElementIdentifier]=this.state.orderForm[formElementIdentifier].value
            }
         const order = {
-            ingredients:this.props.ingredients,
+            ingredients:this.props.ings,
             price:this.props.price,
             orderData:formData
         }
@@ -202,5 +205,11 @@ class ContactData extends Component{
     }
 }
 
+const mapStateToProps = state =>{
+    return{
+        ings:state.ingredients,
+        price:state.totalPrice
+    }
+}
 
-export default ContactData
+export default connect(mapStateToProps)(ContactData)
