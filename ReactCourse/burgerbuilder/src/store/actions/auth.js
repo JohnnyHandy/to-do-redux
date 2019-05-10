5413c2df83ae4cf4544ef35431ff8ccaf5e19df4
Roger Peixoto Duarte <duarterogerpeixoto@gmail.com>
2019-05-10 16:53:02 -0300

    improved code

--- ReactCourse/burgerbuilder/src/store/actions/auth.js
+++ ReactCourse/burgerbuilder/src/store/actions/auth.js
@@ -53,7 +53,6 @@ export const auth =(email,password,isSignup)=>{
         }
         axios.post(url, authData)
         .then(response=>{
-            console.log(response);
             const expirationDate = new Date(new Date().getTime()+response.data.expiresIn*1000)
             localStorage.setItem('token',response.data.idToken);
             localStorage.setItem('expirationDate',expirationDate);
@@ -82,7 +81,7 @@ export const authCheckState = ()=>{
         } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
             if(expirationDate <= new Date()){
-                dispatch()
+                dispatch(logout())
             } else{
                 const userId = localStorage.getItem('userId')
                 dispatch(authSuccess(token,userId));
