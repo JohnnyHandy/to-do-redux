aae99cb0fab6d38e874de959d9065800389497dc
Roger Peixoto Duarte <duarterogerpeixoto@gmail.com>
2019-05-18 07:13:02 -0300

    added tests and deployed to firebase

--- ReactCourse/burgerbuilder/src/store/reducers/auth.js
+++ ReactCourse/burgerbuilder/src/store/reducers/auth.js
@@ -3,7 +3,7 @@ import {updateObject} from '../../shared/utility'
 
 const initialState={
     token:null,
-    user:null,
+    userId:null,
     error:null,
     loading:false,
     authRedirectPath:'/'
